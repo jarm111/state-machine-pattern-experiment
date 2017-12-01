af3db303e8ab75ebc067d2b204d24593332aec7e
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-12-01 14:09:34 +0200

    Created placeholder graphics, borders, player character, vertical movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 1;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb2d.velocity.y));
+    }
+}
