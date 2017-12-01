4993c5707b0ce52b2583f46f04d43f50a0638878
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-12-01 14:18:40 +0200

    Implemented jump controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,22 @@ public class Player : MonoBehaviour {
 
     [SerializeField]
     private float movementSpeed = 1;
+    [SerializeField]
+    private float jumpForce = 1;
 
     private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jumpForce));
+        }
     }
 
     private void FixedUpdate()
