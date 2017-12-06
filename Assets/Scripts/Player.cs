64d5bc1d17adf79a3186ad675d1ac7a08249aa38
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-12-06 07:27:27 +0200

    Implemented prone mechanics. Varied movement speed.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,17 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField]
-    private float movementSpeed = 1;
+    private float walkingSpeed = 10;
     [SerializeField]
-    private float jumpForce = 1;
+    private float jumpForce = 300;
+    [SerializeField]
+    private float crouchingSpeedMultiplier = 0.8f;
+    [SerializeField]
+    private float proneSpeedMultiplier = 0.4f;
 
+    private float currentMovementSpeed;
     private bool isCrouching;
+    private bool isProne;
     private Rigidbody2D rb2d;
     private CapsuleCollider2D capsuleCollider;
 
@@ -16,26 +22,37 @@ public class Player : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        currentMovementSpeed = walkingSpeed;
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            if(!isCrouching && CheckIsGrounded())
-            {
-                Jump();
-            }
-            else
+            if (isCrouching)
             {
                 StandUp();
             }
-            
+            else if (isProne)
+            {
+                Crouch();
+            }
+            else if (CheckIsGrounded())
+            {
+                Jump();
+            }
         }
 
-        if (Input.GetButtonDown("Crouch") && !isCrouching)
+        if (Input.GetButtonDown("Duck") && CheckIsGrounded())
         {
-            Crouch();
+            if (!isCrouching && !isProne)
+            {
+                Crouch();
+            }
+            else if (!isProne)
+            {
+                Prone();
+            } 
         }
 
         //Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - capsuleCollider.size.y - .01f), Vector2.down * .1f, Color.green, 0.5f, false);
@@ -43,12 +60,12 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        Move(currentMovementSpeed);
     }
 
-    private void Move()
+    private void Move(float speed)
     {
-        rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb2d.velocity.y));
+        rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, rb2d.velocity.y));
     }
 
     private void Jump()
@@ -59,13 +76,16 @@ public class Player : MonoBehaviour
     private void Crouch()
     {
         gameObject.transform.localScale = new Vector3(2, 1, 1);
+        currentMovementSpeed = walkingSpeed * crouchingSpeedMultiplier;
         //capsuleCollider.size = new Vector2(1, .5f);
         isCrouching = true;
+        isProne = false;
     }
 
     private void StandUp()
     {
         gameObject.transform.localScale = new Vector3(2, 2, 1);
+        currentMovementSpeed = walkingSpeed;
         //capsuleCollider.size = new Vector2(1, 1);
         isCrouching = false;
     }
@@ -75,7 +95,15 @@ public class Player : MonoBehaviour
         // LayerMask.NameToLayer("Ground")
         bool isGrounded = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - capsuleCollider.size.y - .1f), Vector2.down, .1f, LayerMask.GetMask("Ground"));
         //bool isGrounded = (Mathf.Abs(rb2d.velocity.y) < 0.1);
-        //Debug.Log(isGrounded);
+        Debug.Log(isGrounded);
         return isGrounded;
+    }
+
+    private void Prone()
+    {
+        gameObject.transform.localScale = new Vector3(2, 0.5f, 1);
+        currentMovementSpeed = walkingSpeed * proneSpeedMultiplier;
+        isProne = true;
+        isCrouching = false;
     }
 }
